5cda175e14c695f6543d34ee4ff7adbe7d682931
ali-hassan-alshahat <ali.hassan.alshahat@gmail.com>
2025-10-20 11:25:22 +0300

    creating featured banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import BannerTop from "./components/BannerTop";
 import DealsBanner from "./components/DealsBanner";
 import DiscountBanner from "./components/DiscountBanner";
 import Featured from "./components/Featured";
+import FeaturedBanner from "./components/FeaturedBanner";
 import HotDeals from "./components/HotDeals";
 import Navbar from "./components/Navbar";
 import PopularProducts from "./components/PopularProducts";
@@ -16,6 +17,7 @@ function App() {
       <DealsBanner />
       <HotDeals />
       <DiscountBanner />
+      <FeaturedBanner />
     </>
   );
 }
