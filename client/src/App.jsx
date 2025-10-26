988253cb073a16a003b004a263b3c217a87d484a
ali-hassan-alshahat <ali.hassan.alshahat@gmail.com>
2025-10-26 06:36:10 +0300

    creating the rest of the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/Layout";
 import Shop from "./pages/Shop";
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
+import { Contact } from "lucide-react";
+import Faq from "./pages/Faq";
+import ProductDetails from "./pages/ProductDetails";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
 
 function App() {
   let routers = createBrowserRouter([
@@ -14,6 +21,13 @@ function App() {
           element: <Home />,
         },
         { path: "/shop", element: <Shop /> },
+        { path: "/blog", element: <Blog /> },
+        { path: "/about", element: <About /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "/faq", element: <Faq /> },
+        { path: "/product/:id", element: <ProductDetails /> },
+        { path: "/signin", element: <SignIn /> },
+        { path: "/register", element: <Register /> },
       ],
     },
   ]);
