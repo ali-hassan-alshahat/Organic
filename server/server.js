ad86b4ff0ec830c6803f4a3a0b2e5dafbd096004
Ali Hassan <ali.hassan.alshahat@.com>
2026-03-07 19:50:20 +0200

    fixing vercel

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://organic-psi-blue.vercel.app",
+  "https://organic-client-two.vercel.app",
 ].filter(Boolean);
 
 app.use(
